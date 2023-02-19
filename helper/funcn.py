3376097e306c6bb5ed4d1b33e0c9dd43b27d43b5
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-02-19 15:04:35 +0800

    Update funcn.py

--- helper/funcn.py
+++ helper/funcn.py
@@ -18,7 +18,7 @@ COUNT = []
 OK = {}
 
 uptime = dt.now()
-os.system("wget https://telegra.ph/file/75ee20ec8d8c8bba84f02.jpg -O thumb.jpg")
+os.system("wget https://telegra.ph/file/3eb16af72d33292b41571.jpg -O thumb.jpg")
 
 if not os.path.isdir("downloads/"):
     os.mkdir("downloads/")
